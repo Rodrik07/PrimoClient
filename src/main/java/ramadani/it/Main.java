b74c09e38eb7f2c109c4c72ab80b131d15f803a0
rodrik <rodrik.ramadani07@gmail.com>
2025-10-06 17:51:44 +0200

    inizio esercizio

--- src/main/java/ramadani/it/Main.java
+++ src/main/java/ramadani/it/Main.java
@@ -1,7 +1,22 @@
 package ramadani.it;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        Socket s = new Socket("localhost", 3000);
+        System.out.println("Connected.");
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+        out.println("hello world");
+        String result = in.readLine();
+        System.out.println("Received: " + result);
+
+        s.close();
     }
 }
