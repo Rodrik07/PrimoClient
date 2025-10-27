2957f3910d7b42ebe71fe7d590bc128a4f5b0e2d
lai2-24 <lai2-24@itismeucci.com>
2025-10-27 08:41:36 +0100

    inizio esercizio

--- src/main/java/ramadani/it/Main.java
+++ src/main/java/ramadani/it/Main.java
@@ -8,15 +8,15 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class Main {
-    public static void main(String[] args) throws UnknownHostException, IOException {
-        Socket s = new Socket("localhost", 3000);
+    public static void main(String[] args) throws UnknownHostException, IOException {  
+        Socket socket = new Socket("localhost", 3000);
         System.out.println("Connected.");
-        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
         out.println("hello world");
         String result = in.readLine();
         System.out.println("Received: " + result);
 
-        s.close();
+        socket.close();
     }
 }
